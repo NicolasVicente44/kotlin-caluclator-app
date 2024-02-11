af2db03da9992a467d7d02c8a92088075729645f
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-02-11 15:37:24 -0500

    Inital commit, cloned assigmnet 1 into assimgnet 2

---
+++ Projects/app/src/main/java/ca/georgiancollege/comp3025_w24_assignment1_calculator_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package ca.georgiancollege.comp3025_w24_assignment1_calculator_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ Projects/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "COMP3025-W24-Assignment1-Calculator-App"
+include(":app")
