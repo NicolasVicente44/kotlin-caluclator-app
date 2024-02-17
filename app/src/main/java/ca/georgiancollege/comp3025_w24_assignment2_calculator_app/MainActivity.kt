80dab0bcc59fff7375efc2f44deb0a95c5296a66
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-02-17 14:09:26 -0500

    internal documentation to main acitivty file

--- app/src/main/java/ca/georgiancollege/comp3025_w24_assignment2_calculator_app/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_w24_assignment2_calculator_app/MainActivity.kt
@@ -1,7 +1,12 @@
 //COMP3025-W24-Assignment2-Calculator-App-MainActivity.kt
+//Version 1.0
 //Nicolas Vicente
 //200539594
 //February 11, 2024
+//This is the functional version of the calculator app, building upon the ui of the first assignment.
+//The program utilizes a calculator class independent of the main activity file to handle various calculations and on click listeners
+//The main activity file then instantiates an instance of the calculator and uses view binding to link it to the UI form the activity main layout file
+
 
 package ca.georgiancollege.comp3025_w24_assignment2_calculator_app
 
