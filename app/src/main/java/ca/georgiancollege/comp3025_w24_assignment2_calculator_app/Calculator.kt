047e6096efcd3a28c260ec32acd17c252c0cfffb
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-02-16 20:45:47 -0500

    added addition and subtraction functionality and framework for additional functionality

--- app/src/main/java/ca/georgiancollege/comp3025_w24_assignment2_calculator_app/Calculator.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_w24_assignment2_calculator_app/Calculator.kt
@@ -1,16 +1,25 @@
 package ca.georgiancollege.comp3025_w24_assignment2_calculator_app
-import android.util.Log
+
 import android.view.View
 import ca.georgiancollege.comp3025_w24_assignment2_calculator_app.databinding.ActivityMainBinding
 
+
+
+
 class Calculator(binding: ActivityMainBinding)
 {
-    private var m_resultLabelValue: String = ""
+    private var m_resultLabelValue: String
     private var m_binding: ActivityMainBinding
+    private var m_lhs: String
+    private var m_active_operation: String
+
     init
     {
         this.m_binding = binding
         this.m_resultLabelValue = ""
+        this.m_lhs = ""
+        this.m_active_operation = ""
+
         initializeOnClickListeners()
     }
 
@@ -46,31 +55,72 @@ class Calculator(binding: ActivityMainBinding)
 
     private fun processOperatorButtons(view: View)
     {
-        Log.i("operator buttons", view.tag.toString())
-        this.m_resultLabelValue += view.tag.toString()
-        this.m_binding.resultLabel.text = this.m_resultLabelValue
+        if(m_lhs.isEmpty() && this.m_resultLabelValue.isNotEmpty())
+        {
+            this.m_lhs = this.m_resultLabelValue
+            this.m_resultLabelValue = ""
+            this.m_active_operation = view.tag.toString()
+        }
+        else if(this.m_lhs.isNotEmpty() && this.m_resultLabelValue.isEmpty())
+        {
+            this.m_active_operation = view.tag.toString()
+        }
+        else if(this.m_lhs.isNotEmpty() && this.m_resultLabelValue.isNotEmpty())
+        {
+            // compute the result based ont the last operator selected
+            when(this.m_active_operation) {
+                "multiply" -> {
+
+                }
+
+                "divide" -> {
+
+                }
+
+                "+" -> {
+                    this.m_lhs = add(this.m_lhs, this.m_resultLabelValue)
+                    this.m_resultLabelValue = ""
+                    this.m_binding.resultLabel.text = this.m_lhs;
+                }
+
+                "-" -> {
+                    this.m_lhs = subtract(this.m_lhs, this.m_resultLabelValue)
+                    this.m_resultLabelValue = ""
+                    this.m_binding.resultLabel.text = this.m_lhs;
+                }
+
+                "equals" -> {
+
+                }
+            }
+            // update the last operation
+            this.m_active_operation = view.tag.toString()
+        }
+
     }
 
     private fun processExtraButtons(view: View)
     {
-        when (view.tag.toString()) {
-            "clear" -> {
-                this.m_resultLabelValue = ""
-                this.m_binding.resultLabel.text = "0"
-            }
-            "backspace" -> {
+        when(view.tag.toString())
+        {
+            "backspace" ->
+            {
 
                 this.m_resultLabelValue = this.m_resultLabelValue.dropLast(1)
                 this.m_binding.resultLabel.text = this.m_resultLabelValue
-
-                if (this.m_resultLabelValue.isEmpty() || this.m_resultLabelValue == "-")
+                if(this.m_resultLabelValue.isEmpty() || this.m_resultLabelValue == "-")
                 {
                     this.m_resultLabelValue = ""
                     this.m_binding.resultLabel.text = "0"
                 }
             }
-            "+-" -> {
-                if(this.m_resultLabelValue.isNotEmpty() && this.m_resultLabelValue != "0")
+            "clear" ->
+            {
+                clear()
+            }
+            "+-" ->
+            {
+                if(this.m_resultLabelValue.isNotEmpty())
                 {
                     if(this.m_resultLabelValue.contains("-"))
                     {
@@ -80,44 +130,106 @@ class Calculator(binding: ActivityMainBinding)
                     {
                         this.m_resultLabelValue = "-" + this.m_resultLabelValue
                     }
-
                     this.m_binding.resultLabel.text = this.m_resultLabelValue
                 }
             }
         }
-
-        Log.i("extra buttons", view.tag.toString())
     }
 
-
-
-    private fun processNumberButtons(view: View) {
-        when (view.tag.toString()) {
+    private fun processNumberButtons(view: View)
+    {
+        when (view.tag.toString())
+        {
             "." -> {
-                if (!this.m_resultLabelValue.contains(".")) {
-                    if (this.m_resultLabelValue.isEmpty()) {
+                if(!this.m_resultLabelValue.contains("."))
+                {
+                    if(this.m_resultLabelValue.isEmpty())
+                    {
                         this.m_resultLabelValue = "0."
-                    } else {
+                    }
+                    else
+                    {
                         this.m_resultLabelValue += view.tag.toString()
                     }
-                } else {
-                    this.m_resultLabelValue += ""
-                }
-            }
-            "0" -> {
-                if (this.m_resultLabelValue != "0" && !this.m_resultLabelValue.startsWith("0.")) {
-                    this.m_resultLabelValue += view.tag.toString()
                 }
             }
             else -> {
-                if (this.m_resultLabelValue == "0") {
-                    this.m_resultLabelValue = ""
-                }
                 this.m_resultLabelValue += view.tag.toString()
             }
         }
-        Log.i("number buttons", view.tag.toString())
+
         this.m_binding.resultLabel.text = this.m_resultLabelValue
+    }
+
+    /**
+     * This function clears the in-memory values for the LHS and the RHS
+     *
+     * @return [Unit]
+     */
+    private fun clear():Unit
+    {
+        this.m_resultLabelValue = ""
+        this.m_lhs = ""
+        this.m_active_operation = ""
+        this.m_binding.resultLabel.text = "0"
+    }
+
+    /**
+     * This function subtracts the rhs from the lhs and returns a string representation of the result
+     *
+     * @param lhs [String]
+     * @param rhs [String]
+     * @return [String]
+     */
+    private fun subtract(lhs: String, rhs: String): String
+    {
+        var LHS = lhs
+        var RHS = rhs
+
+        if(LHS.isEmpty())
+        {
+            LHS = "0"
+        }
+
+        if(RHS.isEmpty())
+        {
+            RHS = "0"
+        }
+
+        if(LHS.contains(".") || RHS.contains("."))
+        {
+            return (LHS.toFloat() - RHS.toFloat()).toString()
+        }
+        return (LHS.toInt() - RHS.toInt()).toString()
+    }
+
+    /**
+     * This function adds the lhs to the rhs and returns a string representation of the result
+     *
+     * @param lhs [String]
+     * @param rhs [String]
+     * @return [String]
+     */
+    private fun add(lhs: String, rhs: String): String
+    {
+        var LHS = lhs
+        var RHS = rhs
+
+        if(LHS.isEmpty())
+        {
+            LHS = "0"
+        }
+
+        if(RHS.isEmpty())
+        {
+            RHS = "0"
+        }
+
+        if(LHS.contains(".") || RHS.contains("."))
+        {
+            return (LHS.toFloat() + RHS.toFloat()).toString()
+        }
+        return (LHS.toInt() + RHS.toInt()).toString()
     }
 
 
--- app/src/main/java/ca/georgiancollege/comp3025_w24_assignment2_calculator_app/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_w24_assignment2_calculator_app/MainActivity.kt
@@ -5,6 +5,7 @@
 
 package ca.georgiancollege.comp3025_w24_assignment2_calculator_app
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ca.georgiancollege.comp3025_w24_assignment2_calculator_app.databinding.ActivityMainBinding
